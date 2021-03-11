1cc6a88d5d723ac257951253a76a565793c8c7a9
carinalsilverio <carinalsilverio@users.noreply.github.com>
2021-03-10 22:43:22 -0300

    funcionamento refatorado

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
 import DropdownMenu from './modules/dropdown-menu.js';
-import initMenuMobile from './modules/menu-mobile.js';
-import initFuncionamento from './modules/funcionamento.js';
+import MenuMobile from './modules/menu-mobile.js';
+import Functionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
 import ScrollAnima from './modules/scroll-anima.js';
@@ -31,9 +31,13 @@ scrollAnima.init();
 const dropdownMenu = new DropdownMenu('[data-dropdown]');
 dropdownMenu.init();
 
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
 
-initMenuMobile();
-initFuncionamento();
+const funcionamento = new Functionamento('[data-semana]', 'aberto');
+console.log(funcionamento);
+
+funcionamento.init();
 
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
 
